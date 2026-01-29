be94f88fe3ccf338e03bcda0af6636241eeda4c6
AndreaFraboni <andrea.fraboni@gmail.com>
2026-01-29 20:58:53 +0100

    starting project

---
+++ Assets/_Project/Scripts/UI/UIManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    private bool isPaused = false;
+    AudioManager _audioManager;
+
+    private void Awake()
+    {
+        if (_audioManager == null) _audioManager = FindAnyObjectByType<AudioManager>();
+
+        _audioManager.UpdateSlider();
+
+
+    }
+    private void Start()
+    {
+        _audioManager.PlayMusic("ThemeGame");
+    }
+
+    public void PlayClickSound()
+    {
+        _audioManager.PlaySFX("MouseClickSound");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        _audioManager.StopAllAudioSource();
+
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1.0f;
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+		     Application.Quit();
+#endif
+        }
+    }
+    public void SaveOptions()
+    {
+        Debug.Log("le opzioni sono state salvate !!!");
+    }
+}
