3046fbe3031b7a1c4141d06bc41e0ec2bca329fd
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-31 11:58:00 +0100

    Time Manager added and activated

---
+++ Assets/_Project/Scripts/Utils/Timer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] private float _countDown = 600f; // Durata del timer in secondi = 10 minuti
+    [SerializeField] private float _currentTime;
+    [SerializeField] private TextMeshProUGUI _currentTimetext;
+
+    private void Awake()
+    {
+        _currentTimetext.text = $"{(int)_currentTime} s";
+        _currentTime = _countDown;
+    }
+
+    private void Update()
+    {
+
+        _currentTime -= Time.deltaTime;
+        TimeManager();
+
+        if (_currentTime <= 0)
+        {
+            _currentTime = 0;
+
+            Debug.Log("TEMPO SCADUTO !!!");
+
+            Invoke("GameOver", 1);
+        }
+
+    }
+
+    private void TimeManager()
+    {
+        int secondiTrascorsi = (int)_currentTime;
+        _currentTimetext.text = $"{secondiTrascorsi} s";
+    }
+
+
+    public void GameOver()
+    {
+        Debug.Log("GAME OVER !!!");
+        // STOP all music ancd sfx now not play ==> isPlaying = false;
+        // game in PAUSE ==> isPaused = true;
+        Time.timeScale = 0; 
+
+        // PLAY DEATH MUSIC
+        // Show Game Over
+    }
+
+
+}
+
+
+
