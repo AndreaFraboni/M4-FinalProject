9f24b57a5c6ac2c32cbd6825d2892c7e9a8355b1
unknown <andrea.fraboni@gmail.com>
2026-02-01 11:50:48 +0100

    Player can move and jump without animations

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -4,8 +4,127 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("HUD")]
     [SerializeField] private Image _bar_lifeBarFillable;
     [SerializeField] private TextMeshProUGUI _lifeText;
+
+    [Header("Player movement parameters")]
+    [SerializeField] private float _speed = 6.0f;
+    [SerializeField] private float _smooth = 10f;
+    [SerializeField] private float _jumpForce = 5f;
+
+    private Rigidbody _rb;
+
+    private float horizontal, vertical = 0f;
+
+    private Vector3 currentDirection = Vector3.zero;
+
+
+    private Camera _cam;
+    private Ray _ray;
+
+    private bool isAlive = true;
+    public bool isGrounded = false;
+    private bool isJump = false;
+    private bool isRunning = false;
+
+    private void Awake()
+    {
+        if (_rb == null) _rb = GetComponent<Rigidbody>();
+
+        _cam = Camera.main;
+    }
+
+    void Update()
+    {
+        CheckInput();
+        CheckRun();
+        CheckJump();
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (isJump) Jump();
+
+    }
+
+    private void CheckInput()
+    {
+        if (!isAlive) return;
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 targetDirection = Vector3.zero;
+        targetDirection = _cam.transform.forward * vertical + _cam.transform.right * horizontal;
+        targetDirection.y = 0f;
+
+        if (targetDirection.magnitude > 0.01f) targetDirection.Normalize();
+
+        currentDirection = Vector3.Lerp(currentDirection, targetDirection, _smooth * Time.deltaTime);
+    }
+
+    private void CheckRun()
+    {
+        if (!isAlive) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
+    }
+
+    private void CheckJump()
+    {
+        if (!isAlive) return;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJump = true;
+        }
+    }
+
+
+    private void Move()
+    {
+        if (!isAlive) return;
+
+        if (isRunning)
+        {
+            Vector3 current = _rb.velocity;
+            Vector3 desired = currentDirection * _speed * 2;
+            _rb.velocity = new Vector3(desired.x, current.y, desired.z);
+        }
+        else
+        {
+            Vector3 current = _rb.velocity;
+            Vector3 desired = currentDirection * _speed;
+            _rb.velocity = new Vector3(desired.x, current.y, desired.z);
+        }
+    }
+
+    private void Jump()
+    {
+        if (!isAlive) return;
+
+        isJump = false;
+        if (isRunning) isRunning = false;
+
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+
+
+
+
 
 
 
